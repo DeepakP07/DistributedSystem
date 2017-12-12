c7006036a97a5bd961b91a9531f076f37f58f355
Deepak <32619470+DeepakP07@users.noreply.github.com>
2017-12-12 17:18:11 +0000

    Started from scratch again.
    Started building DFS again from scratch due to some errors in earlier code.

--- client.py
+++ client.py
@@ -1,45 +1,146 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec  6 13:12:41 2017
+Created on Tue Dec 9 16:13:24 2017
 
 @author: deepa
 """
 
-import sys
 import socket
-import struct
-import random
+import sys
+from time import sleep
+from random import randrange
 
-class Client():
+def get_port(data):
+	# connect to socket
+	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	soc.connect(("0.0.0.0", int(sys.argv[1])))
+	soc.settimeout(2)
 
-	#List of valid commands
-	commands = ["READ FILE", "WRITE FILE", "PWDIR", "CHDIR", "LS", "QUIT"]
+	# send data...
+	print "Requesting port for: \"" + data + "\""
+	soc.sendall(data)
 
-	def __init__(self, port):
+	soc.settimeout(2)
 
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.settimeout(1.5)
-		# Convert an IP address from 32-bit packed binary format to string format and Generate a random color hex
-		self.ip = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))     
-		self.port = port
-		s.connect(("0.0.0.0", self.port))
+	# printing the received response
+	received = soc.recv(4096)
+	print "Port Rec: \"{}\"".format(received)
+	parser = received.splitlines()
+ 	port = parser[0].split(' ',1)[1]
+	return port
 
-		valid = False
 
-		while 1:
+if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+    sys.exit("Port number required")
 
-			while not valid:
-				self.message = raw_input(">: ")
-				if any(x in self.message for x in self.commands):
-					valid = True
-				else:
-					print ("Invalid Command\n")
-			s.sendall(self.message)
-			if self.message == "QUIT":
-				sys.exit()
-			try:
-				recv_data = s.recv(4096)
-				print (recv_data + '\n')
-				valid = False;
-			except socket.timeout:
-				print ("Something went wrong\n")
+# set GET message
+data1 = "UPLOAD: foldername1/room1.txt\nUploading the file\n\n"
+data2 = "UPLOAD: foldername1/room2.txt\nUploaing the file\n\n"
+data3 = "UPLOAD: foldername2/room3.txt\nUploading the file\n"
+data4 = "UPDATE: foldername1/room1.txt\nUpdating the  file\n\n"
+data5 = "UPDATE: foldername1/room2.txt\nUpdating a file\n\n"
+data6 = "UPDATE: foldername2/room3.txt\nUpdating a file\n\n"
+data7 = "DOWNLOAD: foldername1/room1.txt\n\n\n"
+data8 = "DOWNLOAD: foldername1/room2.txt\n\n\n"
+data9 = "DOWNLOAD: foldername2/room3.txt\n\n\n"
+
+#    ULOADING FILES
+
+# send data
+print "Sent: \"" + data1 + "\""
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect(("0.0.0.0", int(get_port(data1))))
+r.settimeout(2)
+r.sendall(data1)
+
+# print received response
+received = r.recv(4096)
+
+print "Rec: \"{}\"".format(received)
+# wait for key press
+raw_input("Press Enter to continue...")
+
+# send data
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect(("0.0.0.0", int(get_port(data2)))) 
+r.settimeout(2)
+
+r.sendall(data2)
+
+# print received response
+received = r.recv(4096)
+print "Rec: \"{}\"".format(received)
+
+# wait for key press
+raw_input("Press Enter to continue...")
+
+# send data
+print "Sent: \"" + data3 + "\""
+
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect(("0.0.0.0", int(get_port(data3))))
+r.settimeout(2)
+
+r.sendall(data3)
+# print received response
+received = r.recv(4096)
+print "Rec: \"{}\"".format(received) # READ FILES
+# wait for key press
+raw_input("Press Enter to continue...")
+
+# send data
+print "Sent: \"" + data7 + "\""
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect(("0.0.0.0", int(get_port(data7))))
+r.settimeout(2)
+r.sendall(data7)
+
+# print received response
+received = r.recv(4096)
+
+print "Rec: \"{}\"".format(received)
+# wait for key press
+raw_input("Press Enter to continue...")
+
+# send data
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect(("0.0.0.0", int(get_port(data8))))
+r.settimeout(2)
+
+r.sendall(data8)
+
+# print received response
+received = r.recv(4096)
+print "Rec: \"{}\"".format(received)
+
+# wait for key press
+raw_input("Press Enter to continue...")
+
+# send data
+print "Sent: \"" + data9 + "\""
+
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect(("0.0.0.0", int(get_port(data9))))
+r.settimeout(2)
+
+r.sendall(data9)
+# print received response
+received = r.recv(4096)
+print "Rec: \"{}\"".format(received)
+
+
+#    UPDATING FILES
+# wait for key press
+raw_input("Press Enter to continue...")
+
+# send data
+print "Sent: \"" + data4 + "\""
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect(("0.0.0.0", int(get_port(data4))))
+r.settimeout(2)
+r.sendall(data4)
+
+# print received response
+received = r.recv(4096)
+
+print "Rec: \"{}\"".format(received)
